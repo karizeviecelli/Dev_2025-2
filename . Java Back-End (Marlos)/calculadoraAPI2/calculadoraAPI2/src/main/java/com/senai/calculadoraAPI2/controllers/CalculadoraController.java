a9bde64b6e4673a3bd6d1bdb5056d4c15fc97305
matheus_leite <matheusleite1216@gmail.com>
2025-08-12 23:12:43 -0300

    calculadoraApi2 atualização dos post das operações matemáticas

--- . Java Back-End (Marlos)/calculadoraAPI2/calculadoraAPI2/src/main/java/com/senai/calculadoraAPI2/controllers/CalculadoraController.java
+++ . Java Back-End (Marlos)/calculadoraAPI2/calculadoraAPI2/src/main/java/com/senai/calculadoraAPI2/controllers/CalculadoraController.java
@@ -3,16 +3,32 @@ package com.senai.calculadoraAPI2.controllers;
 import com.senai.calculadoraAPI2.dtos.CalculadoraDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/calc")
 public class CalculadoraController {
-    CalculadoraDto resultado;
 
     @PostMapping("/somar")
     public ResponseEntity<CalculadoraDto> somar(@RequestBody CalculadoraDto calc){
-        Double r = calc.getNum1()+ calc.getNum2();
-        resultado.setResultado(r);
-        return ResponseEntity.ok(resultado);
+        calc.setResultado(calc.getNum1() + calc.getNum2());
+        return ResponseEntity.ok(calc);
+    }
+
+    @PostMapping("/subtrair")
+    public ResponseEntity<CalculadoraDto> subtrair(@RequestBody CalculadoraDto calc){
+        calc.setResultado(calc.getNum1() - calc.getNum2());
+        return ResponseEntity.ok(calc);
+    }
+
+    @PostMapping("/multiplicar")
+    public ResponseEntity<CalculadoraDto> multiplicar(@RequestBody CalculadoraDto calc){
+        calc.setResultado(calc.getNum1() * calc.getNum2());
+        return ResponseEntity.ok(calc);
+    }
+
+    @PostMapping("/dividir")
+    public ResponseEntity<CalculadoraDto> dividir(@RequestBody CalculadoraDto calc){
+        calc.setResultado(calc.getNum1() / calc.getNum2());
+        return ResponseEntity.ok(calc);
     }
 }
+
