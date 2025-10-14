766a2acda644a8bf3dd04f1bdebe270ae7dcfaf5
BrunoRprogm <brunoramonnn986@gmail.com>
2025-10-14 18:58:39 -0300

    Delete src directory

--- src/main/java/com/senai/crud/repositories/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senai.crud.repositories;
-
-import com.senai.crud.models.CategoriaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
-}
